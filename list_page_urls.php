0796d1f5669c293e2d4808f01b6fdac0f0a38102
Jasti Sri Radhe Shyam <jastisriradheshyam@users.noreply.github.com>
2018-03-17 22:33:07 +0530

    Update list_page_urls.php
    added comment and indented them

--- list_page_urls.php
+++ list_page_urls.php
@@ -2,22 +2,22 @@
 echo "<br/>";
 echo "<b>Internal and External Link lists : </b>";
 echo "<br/><br/>";
-//function for checking link is a internal link or external link
+// function for checking link is a internal link or external link
 function checkLink($url,$gathered_from) {
     if(filter_var($url, FILTER_VALIDATE_URL)){
-        //parses the target url and breakes it into different components
+        // parses the target url and breakes it into different components
         $component = parse_url($gathered_from);
-        //starts with www then . then some characters then . all matching is case insensitive
-        //removes the www if it is present by $1 captures the group (.+\.) and repleaces with orignal host
+        // starts with www then . then some characters then . all matching is case insensitive
+        // removes the www if it is present by $1 captures the group (.+\.) and repleaces with orignal host
         // basically hostname without wwww
         $component['host'] = preg_replace('#^www\.(.+\.)#i', '$1', $component['host']);
-        //parses the urls in target url page and breakes it into different components
+        // parses the urls in target url page and breakes it into different components
         $new_component = parse_url($url);
-        //starts with www then . then some characters then . all matching is case insensitive
-        //removes the www if it is present by $1 captures the group (.+\.) and repleaces with orignal host
+        // starts with www then . then some characters then . all matching is case insensitive
+        // removes the www if it is present by $1 captures the group (.+\.) and repleaces with orignal host
         // basically hostname without wwww
         $new_component['host'] = preg_replace('#^www\.(.+\.)#i', '$1', $new_component['host']);
-        //if the host of target url and url in target url page same it is Internal Link else external link
+        // if the host of target url and url in target url page same it is Internal Link else external link
         if(strpos($component['host'],$new_component['host'])||strpos($new_component['host'],$component['host'])||$new_component['host'] == $component['host'])
         {
         echo "<b>Internal Link: </b> ";
@@ -32,7 +32,7 @@ function checkLink($url,$gathered_from) {
         }
     }
 }
-//$webaddress = $_POST["website"];
+// $webaddress = $_POST["website"];
 $target_url = 'https://cruxonearth.blogspot.in/2017/10/options-styles-of-commands-crux.html';
 // make the cURL request to $target_url
 $ch = curl_init();
@@ -41,9 +41,9 @@ curl_setopt($ch, CURLOPT_FAILONERROR, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-//curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+// curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 $html= curl_exec($ch);
-//Error check if site is not available
+// error check if site is not available
 if (!$html) {
     echo "<br />cURL error number:" .curl_errno($ch);
     echo "<br />cURL error:" . curl_error($ch);
@@ -51,8 +51,9 @@ if (!$html) {
 }
 // parse the html into a DOMDocument
 $dom = new DOMDocument();
+// loads the downloaded html to dom variable to parse in DOM members
 @$dom->loadHTML($html);
-//grab titles and all the things
+// grab titles and all the things
 // grab all the on the page
 $xpath = new DOMXPath($dom);
 // get all the data in a (anchor tag) in html>body path / DOM
